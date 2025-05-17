981ba501a21aa18f1af3f2b9b973b955aabd88fe
YargenFlargen <mrhemenway99@gmail.com>
2025-05-17 17:45:10 -0500

    CreatePumps

---
+++ src/main/java/com/yargenflargen/entry/BetterPumpsLang.java
@@ -0,0 +1,13 @@
+package com.yargenflargen.entry;
+
+import com.yargenflargen.CreateBetterPumps;
+
+import static com.simibubi.create.Create.registrate;
+
+public class BetterPumpsLang {
+    static {
+        CreateBetterPumps.REG.addRawLang("itemGroup.better_pumps", "Create: Better Pumps");
+    }
+
+    public static void register() {}
+}
---
+++ src/main/java/com/yargenflargen/entry/CreatePumpsRegistration.java
@@ -0,0 +1,25 @@
+package com.yargenflargen.entry;
+
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.yargenflargen.CreateBetterPumps;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreatePumpsRegistration {
+    public static final CreateRegistrate REGISTRATE = CreateBetterPumps.registrate()
+            .setCreativeTab(CreatePumpsTab.TAB);
+
+
+
+
+    public static void register() {}
+}
+
+
